295881caa4514e948c9b5e6812671c24defbda50
Marco Eidinger <marco.eidinger@sap.com>
2022-01-10 22:00:35 -0800

    add swift-argument-parser and repeat example

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Demo of using Swift Argument Parser and await/async
 
 1. Created package with `swift package init --type executable --name RepeatCommandLineTool` and can be build and executed with `swift run`
+2. Add Swift Argument Parser and Apple's `Repeat` example (sync processing), and can be build and executed, example: `swift run repeat --count 5 HelloWorld`
--- Sources/RepeatCommandLineTool/main.swift
+++ Sources/RepeatCommandLineTool/main.swift
@@ -1,1 +1,26 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct Repeat: ParsableCommand {
+    @Option(help: "The number of times to repeat 'phrase'.")
+    var count: Int?
+
+    @Flag(help: "Include a counter with each repetition.")
+    var includeCounter = false
+
+    @Argument(help: "The phrase to repeat.")
+    var phrase: String
+
+    mutating func run() throws {
+        let repeatCount = count ?? .max
+
+        for i in 1...repeatCount {
+            if includeCounter {
+                print("\(i): \(phrase)")
+            } else {
+                print(phrase)
+            }
+        }
+    }
+}
+
+Repeat.main()
