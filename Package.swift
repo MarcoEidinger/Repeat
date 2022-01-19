c4ea547013e0dbbbf1bff363e2bd1592ed5966c6
Marco Eidinger <marco.eidinger@sap.com>
2022-01-19 15:08:54 -0800

    option to avoid error dyld: Library not loaded: @rpath/libswift_Concurrency.dylib"
    can be encountered when running command-line tool
    from Xcode on < macOS 12.0

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
 let package = Package(
     name: "RepeatCommandLineTool",
@@ -19,9 +20,78 @@ let package = Package(
             name: "RepeatCommandLineTool",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]),
+            ]
+        ),
         .testTarget(
             name: "RepeatCommandLineToolTests",
             dependencies: ["RepeatCommandLineTool"]),
     ]
 )
+
+// IMPORTANT: enable the following function call if you encounter the error
+//    `dyld: Library not loaded: @rpath/libswift_Concurrency.dylib`
+
+//hookInternalSwiftConcurrency()
+
+func hookInternalSwiftConcurrency() {
+    let isFromTerminal = ProcessInfo.processInfo.environment.values.contains("/usr/bin/swift")
+    if !isFromTerminal {
+        package.targets.first?.addLinkerSettingUnsafeFlagRunpathSearchPath()
+    }
+}
+
+extension PackageDescription.Target {
+    func addLinkerSettingUnsafeFlagRunpathSearchPath() {
+        linkerSettings = [linkerSetting]
+    }
+
+    private var linkerSetting: LinkerSetting {
+        guard let xcodeFolder = Executable("/usr/bin/xcode-select")("-p") else {
+            fatalError("Could not run `xcode-select -p`")
+        }
+
+        let toolchainFolder = "\(xcodeFolder.trimmed)/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift-5.5/macosx"
+
+        return .unsafeFlags(["-rpath", toolchainFolder])
+    }
+}
+
+extension String {
+    var trimmed: String { trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) }
+}
+
+private struct Executable {
+    private let url: URL
+
+    init(_ filePath: String) {
+        url = URL(fileURLWithPath: filePath)
+    }
+
+    func callAsFunction(_ arguments: String...) -> String? {
+        let process = Process()
+        process.executableURL = url
+        process.arguments = arguments
+
+        let stdout = Pipe()
+        process.standardOutput = stdout
+
+        process.launch()
+        process.waitUntilExit()
+
+        return stdout.readStringToEndOfFile()
+    }
+}
+
+extension Pipe {
+    func readStringToEndOfFile() -> String? {
+        let data: Data
+        if #available(OSX 10.15.4, *) {
+            data = (try? fileHandleForReading.readToEnd()) ?? Data()
+        } else {
+            data = fileHandleForReading.readDataToEndOfFile()
+        }
+
+        return String(data: data, encoding: .utf8)
+    }
+}
+
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Demo of using Swift Argument Parser and await/async
 2. Commit [295881c](https://github.com/MarcoEidinger/Repeat/commit/295881caa4514e948c9b5e6812671c24defbda50): I added Swift Argument Parser and Apple's `Repeat` example (sync processing). Build and run with `swift run repeat --count 5 HelloWorld`
 3. Commit [929075b](https://github.com/MarcoEidinger/Repeat/commit/929075b3cf15db92f3dfba296397083ce610142a): I used `@main` attribute and therefore had to rename `main.swift` file to avoid the error `'main' attribute cannot be used in a module that contains top-level code`
 4. Commit [3909324](https://github.com/MarcoEidinger/Repeat/commit/39093245f3ff30e3bb42e70f181ec1435d3706f4): I added new code artifacts to convert the example to use async/await
+
+> If you encounter the error `dyld: Library not loaded: @rpath/libswift_Concurrency.dylib` when running the command-line tool from within Xcode on a macOS version < 12.0 then you can enable the commented code in `Package.swift` (introduced with commit [26c67af](https://github.com/MarcoEidinger/Repeat/commit/26c67af70fdb88287445821853f5fe61e40f1237))
